d1d6f975462afb591f89e33ba2c29eb1c42990c7
MingliangChen <chen.mingliang@nghinsights.com>
2023-10-24 17:52:04 +0800

    add  pandas  excel  demo

---
+++ pandas_excel/python_pandas_excel.py
@@ -0,0 +1,74 @@
+
+import pandas as pd
+from pandas import DataFrame
+
+# read
+
+
+def read():
+    file_path = r'./python_pandas_excel.xlsx'
+    df = pd.read_excel(file_path, sheet_name="Sheet1")
+    print(df)
+    print("----------")
+    print(df.head())
+    print("----------")
+    print(df.columns)
+    print("----------")
+    print(df.index)
+    print("----------")
+    print(df["name"])
+    print("----------")
+    print(df.describe())
+
+# write
+
+
+def write():
+    data = {'name': ['zs', 'ls', 'ww'], 'age': [
+        11, 12, 13], 'gender': ['man', 'man', 'woman']}
+    df = DataFrame(data)
+    df.to_excel('pandas_write_excel.xlsx')
+
+# 编辑excel
+
+
+def edit():
+    file_path = r'./TagBaseInfo信息整理.xlsx'
+    df = pd.read_excel(file_path)
+    name_column = df['水源名称']
+    # print("水源名称列的值为：")
+    print(*name_column, sep=",")
+
+    # 遍历方式一，按行遍历，带索引
+    # for r in df.iterrows():
+    #   print(r)
+
+    # 遍历方式二，一行为一个元组
+    # 新列的值放到一个列表中
+    new_column_value = []
+    for row in df.itertuples():
+        # print(row)
+        name = getattr(row, "水源名称", "")
+        station = getattr(row, "消防站简称", "")
+        link = getattr(row, "联系方式", "")
+        new_value = "水源名称:"+name+",消防站简称:"+station+",联系方式:"+str(link)
+        new_column_value.append(new_value)
+    # print(new_column_value)
+
+    old_columns = df.columns.tolist()
+    old_columns.append("新增一列的名称")
+    new_columns = old_columns
+    df.reindex(columns=new_columns)
+    df["新增一列的名称"] = new_column_value
+    df.to_excel("用已有列计算得到新的列.xlsx")
+
+
+def test():
+    print("This is test def ")
+
+
+if __name__ == "__main__":
+    # read()
+    # write()
+    edit()
+    test()
